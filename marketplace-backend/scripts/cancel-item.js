fc7bb93a5b8c452ab445da628710d06056296f98
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-12-18 14:21:01 +0000

    Moralis Cloud Functions

---
+++ marketplace-backend/scripts/cancel-item.js
@@ -0,0 +1,20 @@
+const TOKEN_ID = 0
+const { moveBlocks } = require("../utils/move-blocks")
+
+async function cancel() {
+  const nftMarketplace = await ethers.getContract("NftMarketplace")
+  const basicNft = await ethers.getContract("BasicNft")
+  const tx = await nftMarketplace.cancelListing(basicNft.address, TOKEN_ID)
+  await tx.wait(1)
+  console.log("NFT Canceled!")
+  if (network.config.chainId == "31337") {
+    await moveBlocks(2, (sleepAmount = 1000))
+  }
+}
+
+cancel()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.log(error)
+    process.exit(1)
+  })
--- marketplace-nextjs-frontend/cloudFunctions/updateActiveItems.js
+++ marketplace-nextjs-frontend/cloudFunctions/updateActiveItems.js
@@ -26,3 +26,33 @@ Moralis.Cloud.afterSave("ItemListed", async () => {
         await activeItem.save()
     }
 })
+
+Moralis.Cloud.afterSave("ItemCancelled", async (request) => {
+    const confirmed = request.object.get("confirmed")
+    const logger = Moralis.Cloud.getLogger()
+    logger.info(`Marketplace | Object:  ${request.object}`)
+    if (confirmed) {
+        const ActiveItem = Moralis.Object.extend("ActiveItem")
+        const query = new Moralis.Query(ActiveItem)
+        query.equalTo("marketplaceAddress", request.object.get("address"))
+        query.equalTo("nftAddress", request.object.get("nftAddress"))
+        query.equalTo("tokenId", request.object.get("tokenId"))
+        logger.info(`Marketplace | Query: ${query}`)
+        const cancelItem = await query.first()
+        logger.info(`Marketplace | CanceledItem: ${cancelItem}`)
+        if (cancelItem) {
+            logger.info(
+                `Deleting ${request.object.get("tokenId")}at address ${request.object.get(
+                    "address"
+                )} since it was canceled`
+            )
+            await cancelItem.destroy()
+        } else {
+            logger.info(
+                `No item found with address ${request.object.get(
+                    "address"
+                )} and tokenId: ${request.object.get("tokenId")}`
+            )
+        }
+    }
+})
