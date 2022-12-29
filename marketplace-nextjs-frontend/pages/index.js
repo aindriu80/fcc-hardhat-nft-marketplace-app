a5dbf29be5c4576dd89723965c76298a10314a41
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2022-12-29 16:45:25 +0000

    Updated to get events from decentralized data structure

--- marketplace-nextjs-frontend/pages/index.js
+++ marketplace-nextjs-frontend/pages/index.js
@@ -1,26 +1,34 @@
 import styles from "../styles/Home.module.css"
 import { useMoralisQuery, useMoralis } from "react-moralis"
 import NFTBox from "../components/NFTBox"
+import networkMapping from "../constants/networkMapping.json"
+import GET_ACTIVE_ITEMS from "../constants/subgraphQueries"
+import { useQuery } from "@apollo/client"
 
 export default function Home() {
-    const { isWeb3Enabled } = useMoralis()
-    const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery
+    const { isWeb3Enabled, chainId } = useMoralis()
+    const chainString = chainId ? parseInt(chainId).toString() : "31337"
+    const marketplaceAddress = networkMapping[chainString].NftMarketplace[0]
+
+    const { loading, error, data: listedNfts } = useQuery(GET_ACTIVE_ITEMS)
+    // const { data: listedNfts, isFetching: fetchingListedNfts } = useMoralisQuery
     // TableName
     // Function for the query
-    "ActiveItem", (query) => query.listed(10).descending("tokenId")
-    console.log(listedNFts)
+
+    // "ActiveItem", (query) => query.listed(10).descending("tokenId")
+    // console.log(listedNFts)
+
     return (
         <div className="container mx-auto">
             <h1 className="px-4 py-4 text-2xl font-bold ">Recently Listed</h1>
             <div className="flex flex-wrap">
                 {isWeb3Enabled ? (
-                    fetchingListedNfts ? (
+                    loading || !listedNfts ? (
                         <div>Loading...</div>
                     ) : (
-                        listedNfts.map((nft) => {
+                        listedNfts.activeItems.map((nft) => {
                             console.log(nft.attributes)
-                            const { price, nftAddress, tokenId, marketplaceAddress, seller } =
-                                nft.attributes
+                            const { price, nftAddress, tokenId, seller } = nft
                             return (
                                 <div>
                                     <NFTBox
